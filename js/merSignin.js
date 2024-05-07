fa8894b135f668db06340a8cac0fd60e0ed8a9aa
ar-zoop <arzoobapna01@gmail.com>
2024-05-07 23:27:21 +0530

    Correct merchant sign in. Add merchant payment history

--- js/merSignin.js
+++ js/merSignin.js
@@ -8,27 +8,28 @@ const signIn =  async(d) => {
     try{
         const response = await axios.request({
             method : 'POST',
-            url : 'http://localhost:3000/api/v1/merchant/signIn',
+            url : 'http://localhost:3000/api/v1/merchants/signin',
             data : d
         })
         console.log(response)
         if (response.status == 200 || response.status==201){
             console.log("Login successful");
-            const jwtToken = json.response.response;
+            console.log(response)
+            const jwtToken = response.data.data;
             localStorage.setItem('jwtToken', jwtToken);
-            localStorage.setItem('phoneNumber', response.phoneNumber);
-            window.location.href = 'dashboard.html';  
+            localStorage.setItem('phoneNumber', d.phoneNumber);
+            window.location.href = 'm_payment_history.html';  
         }
         else{
             console.error('Sign in failed:', response.message);
             errorMessageDiv.textContent = response.message; 
-            errorMessageDiv.style.display = 'block'; 
+            errorMessageDiv.style.display = 'BLOCK'; 
         }
     }
     catch (error){
         console.error("Error:", error)
         errorMessageDiv.textContent = error.message; 
-        errorMessageDiv.style.display = 'block'; 
+        errorMessageDiv.style.display = 'BLOCK'; 
     }
 }
 
---
+++ js/merchantPaymentHistory.js
@@ -0,0 +1,59 @@
+const userPhoneNumber = localStorage.getItem('phoneNumber')
+const jwtToken = localStorage.getItem('jwtToken');
+
+const errorMessageDiv = document.getElementById("errorMessage");
+const container = document.getElementById("transactionBox")
+
+
+const showTransactions = async (d) => {
+	console.log(d)
+	try {
+		const response = await axios.request({
+			method: 'POST',
+			url: 'http://localhost:3000/api/v1/transactions/merchant',
+			data: d,
+			headers: { 'authorization': jwtToken }
+		})
+
+		if (response.status == 200 || response.status == 201) {
+
+			const transactionArray = response.data.data;
+			console.log(transactionArray)
+			
+			for (i = 0; i < transactionArray.length; i++) {
+				let userPhoneNumber = transactionArray[i].userPhoneNumber;
+				let amount = transactionArray[i].amount;
+				let transactionDate = transactionArray[i].date;
+                let time = transactionArray[i].time;
+                let voucherId = transactionArray[i].voucherId;
+                const transactionHtml = `
+                <div>Amount: <span>${amount}</span></div>
+                <div>Date & Time: <span>${transactionDate} ${time}</span></div>
+                <div>Payment from: <span>${userPhoneNumber}</span></div>
+                <hr>
+                `
+                container.innerHTML += transactionHtml;
+				
+			}
+        } else {
+            // DISPLAY NO TRANSACTIONS MADE
+            errorMessageDiv.textContent = "No transactions made. Make a payment"
+            errorMessageDiv.style.display = 'BLOCK';
+        }
+		
+	} catch (error) {
+		console.error("Error:", error)
+		errorMessageDiv.textContent = error.message;
+		errorMessageDiv.style.display = 'BLOCK';
+	}
+}
+
+
+window.addEventListener("load", (e) => {
+	// e.preventDefault();
+	let d= {
+		phoneNumber : userPhoneNumber,
+	}
+	console.log("data object: ", d)
+	showTransactions(d);
+})
